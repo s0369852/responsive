46de5e1995b4de5ab4146674d147aaea4181df3a
s0369852 <s0369852@gmail.com>
2020-09-25 02:50:23 +0900

    메뉴 접기/펼치기

--- js/main.js
+++ js/main.js
@@ -19,6 +19,36 @@
     // 공유하기 버튼을 따로 넣어준 이유는 head태그에 script defer로 넣고 직접 입력했을때, 클래스가(.facebook, .twitter) 정해지기 이전이므로 선택자를 인식하지를 못한다.
     // 그래서 따로 main.js 파일을 만들어 넣어준 것. (html파일에 입력하고 싶다면 body태그의 하단 부분에 script를 입력해주면 된다.)
 
+// 전체 메뉴(접기, 펼치기)
+    $(".btn").click(function(e){
+        e.preventDefault();
+        $(".nav").slideToggle();
+        $(".btn").toggleClass("open");
+        
+        if( $(".btn").hasClass("open") ){
+            // "open"이란 클래스가 있을 때 
+            $(".btn").find("i").attr("class","fa fa-angle-up");
+            // .attr은 속성을 가져올 수도 있고, 변경할 수도 있다.
+        } else {
+            // "open"이란 클래스가 없을 때
+            $(".btn").find("i").attr("class","fa fa-angle-down");
+        }
+    });
+
+    $(window).resize(function(){
+        let wWidth = $(window).width();
+        if(wWidth > 600){
+            $(".nav").removeAttr("style");
+        }
+    });
+    // 전체 메뉴 접고 펼치기를 스크립트로 작업을 해줬기 때문에 display: block상태가 유지가 된다. 
+    // css에는 이미 display: none이라고 되어있지만, 우선순위로 인해서 보이는 상태가 유지되는데,
+    // 이것을 바꿔주려면 다시 스크립트로 작업을 해야한다.
+    // 그래서 .resize로 함수를 만들었다.
+    // 윈도우(브라우저)의 너비가 600보다 작게되면 style(속성)을 지우는 함수이다.
+
+
+
 // 이미지 슬라이더
     $(".slider").slick({
         dots: true,
@@ -37,5 +67,5 @@
 
 // 라이트 박스
     $(".lightbox").lightGallery({
-
     });
+
