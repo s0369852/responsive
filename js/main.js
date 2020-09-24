2b1301890bf122000ed607a837cc05affe78651f
s0369852 <s0369852@gmail.com>
2020-09-24 21:00:14 +0900

    이미지 슬라이드

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// sns 공유하기
     // 공유하기 버튼의 기본 원리 (버튼을 클릭했을 때 윈도우 창이 열린다.)
     //http://www.facebook.com/sharer.php?u={페이지 제목}&t={페이지링크}
     //http://twitter.com/intent/tweet?text={페이지 제목}&url={페이지링크}
@@ -17,3 +18,19 @@
 
     // 공유하기 버튼을 따로 넣어준 이유는 head태그에 script defer로 넣고 직접 입력했을때, 클래스가(.facebook, .twitter) 정해지기 이전이므로 선택자를 인식하지를 못한다.
     // 그래서 따로 main.js 파일을 만들어 넣어준 것. (html파일에 입력하고 싶다면 body태그의 하단 부분에 script를 입력해주면 된다.)
+
+// 이미지 슬라이더
+    $(".slider").slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    autoplay: false
+                }
+            }
+        ]
+    });
